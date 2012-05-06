267d38ee93db6fc9a3c8579329cc85327a2caf6e
Andrew Gruner <andrew@divineflame.com>
2012-05-05 23:29:24 -0700

    adding sorting of posts by date, adding comments, removing jot references

--- lib/NodePub/BlogEngine/PostManager.php
+++ lib/NodePub/BlogEngine/PostManager.php
@@ -9,7 +9,7 @@ use NodePub\BlogEngine\PostMetaParser as Parser;
 
 class PostManager
 {
-    protected $sourceDirs = array();
+    protected $sourceDirs;
     protected $postIndex;
     protected $tags;
     protected $contentFilter;
@@ -17,6 +17,10 @@ class PostManager
   
     function __construct($sourceDirs)
     {
+        # initialize params with defaults
+        $this->sourceDirs = array();
+        $this->sourceFileExtension = 'txt';
+        
         if (is_array($sourceDirs)) {
             foreach ($sourceDirs as $dir) {
                 $this->addSource($dir);
@@ -35,15 +39,22 @@ class PostManager
         if (is_dir($sourcePath)) {
             $this->sourceDirs[] = $sourcePath;
         } else {
-            throw new \Exception(sprintf('Source Path "%s" is Not a Directory', $sourcePath));
+            throw new \Exception(sprintf('Source path "%s" is not a directory', $sourcePath));
         }
     }
 
+    /**
+     * Set the filter passed to Posts for filtering content
+     * @see NodePub\BlogEngine\FilterMarkdown
+     */
     public function setContentFilter($filter)
     {
         $this->contentFilter = $filter;
     }
     
+    /**
+     * Set the extension used for searching for post files
+     */
     public function setSourceFileExtension($ext)
     {
         $this->sourceFileExtension = $ext;
@@ -61,8 +72,7 @@ class PostManager
             ;
         
         # add all source paths to the finder
-        foreach ($this->sourceDirs as $dir)
-        {
+        foreach ($this->sourceDirs as $dir) {
            $files->in($dir);
         }
         
@@ -96,7 +106,8 @@ class PostManager
                 $metadata = $parser->getMetadata();
 
                 $basename = $fileinfo->getBasename('.' . $this->sourceFileExtension);
-
+                
+                # @TODO: refactor to allow different filename/permalink schemas
                 preg_match('/(\d{4})-(\d{2})-(\d{2})-(.+)/', $basename, $matches);
 
                 $metadata['year'] = $matches[1];
@@ -110,6 +121,9 @@ class PostManager
 
                 $posts[$metadata['permalink']] = $metadata;
             }
+            
+            # sort posts in incremental order
+            asort($posts);
 
             $this->postIndex = new ArrayCollection($posts);
         }
@@ -132,13 +146,11 @@ class PostManager
             $recentPosts = $posts->slice(-$offset, $limit);
         }
         
-        if ($expand)
-        {
+        if ($expand) {
             $recentPosts = $this->expandPosts($recentPosts);
         }
         
-        if ($recentPosts instanceOf ArrayCollection)
-        {
+        if ($recentPosts instanceOf ArrayCollection) {
             $recentPosts = $recentPosts->toArray();
         }
 
@@ -274,14 +286,9 @@ class PostManager
      */
     public function savePost(Post $post, $fileContent)
     {
-        $fileName = $this->getFileNameOrCreate($jot);
+        $fileName = $this->getFileNameOrCreate($post);
         
-        // # if Jot title has changed,
-        // # rename the file
-        // if ($jot->isRenamed())
-        // {
-        //     $this->renameJotFile($jot);
-        // }
+        # @TODO: need to allow for saving renamed posts
         
         $file = new \SplFileObject($fileName, 'w+');
         
@@ -293,18 +300,26 @@ class PostManager
             $file->fflush();
             
             return $post;
-        }
-        catch (\Exception $e) {
-            # TODO
+        } catch (\Exception $e) {
+            # @TODO
         }
     }
     
+    /**
+     * Gets a post's filename if it exists,
+     * otherwise creates the filename
+     */
     protected function getFileNameOrCreate($post)
     {
-        if (! isset($post->filename)) {
-            $fileName = sprintf('%s/%s.%s', 
+        if (isset($post->filename)) {
+            $filenName = $post->filename;
+        } else {
+            # @TODO: refactor to allow different filename/permalink schemas
+            $fileName = sprintf('%s/%s-%s-%s.%s',
                 $this->sourceDirs[0],
-                $file->slug,
+                $post->year,
+                $post->month,
+                $post->slug,
                 $this->sourceFileExtension
             );
         }
@@ -313,55 +328,18 @@ class PostManager
     }
     
     /**
-     * Renames a Jot file
-     * 
-     * Infers the previous filename based on the previous slug
+     * Deletes a Post file
      */
-    protected function renameJotFile(Jot $jot)
+    public function deletePost(Post $post)
     {
-        $fileInfo = new \SplFileInfo($jot->getFileName());
-        
-        $previousFileName = sprintf('%s/%s.%s', 
-            $fileInfo->getPath(),
-            $jot->getPreviousSlug(),
-            $fileInfo->getExtension()
-        );
-        
-        if (is_file($previousFileName) && !is_file($jot->getFileName()))
-        {
-            try
-            {
-                rename($previousFileName, $jot->getFileName());
+        $postFile = '';
+        if (is_file($postFile)) {
+            try {
+                unlink($postFile);
                 return true;
-            }
-            catch (Exception $e)
-            {
+            } catch (Exception $e) {
                 return false;
             }
-        }
-        else
-        {
-            return false;
-        }
-    }
-    
-    /**
-     * Deletes a Jot file
-     */
-    public function deleteJot(Jot $jot)
-    {
-        if (is_file($jot->getFileName()))
-        {
-            try
-            {
-                unlink($jot->getFileName());
-                return true;
-            }
-            catch (Exception $e)
-            {
-                return false;
-            }
-            
         }
         
         return false;
